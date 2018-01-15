cd3c2e428ecb4ceaac34bf87c25d49130575b952
tentendayoooon <coreten59@gmail.com>
2018-01-15 14:04:58 +0900

    aiueo9

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+String[] t = new String[]{'ノーマル特攻','エリート特攻','ボス特攻','魔物特攻(赤)','PVP特攻(青)','モンスト特攻(黄)'};
+String[] b = new String[]{'片手剣','二刀流','槍','短剣','弓','頭','鎧','腕','足'};
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -26,6 +29,12 @@ client.on('message', message => {
 	}
 	if (msg.indexOf('おやす') !== -1) {
 		message.reply('ヴァルスミーレ');
+	}
+
+	if (msg === '召喚ボックス') {
+		const rt = new Random();
+		const rb = new Random();
+		message.reply(t[rt.nextInt(6)]+' '+b[rb.nextInt(9)]+' '+'獲得!!');
 	}
 });
 
