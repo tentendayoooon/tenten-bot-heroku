7c27e1d61d5820d67ef43b5c820e869c351b0f2e
tentendayoooon <coreten59@gmail.com>
2018-01-15 11:42:51 +0900

    aiueo3

--- bot.js
+++ bot.js
@@ -6,23 +6,34 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
+		String msg = message.content;
+    if (msg === 'ping') {
+    	message.reply('pong');
+		}
+    if (msg === 'おはよ') {
+    	message.reply('うさぎ_(:3 」∠)');
+		}
+    if (msg === 'こんにちは') {
+    	message.reply('わにわに_(:3 」∠)');
+  	}
+    if (msg === 'こんばんは') {
+    	message.reply('わにわに_(:3 」∠)');
+		}
+
+		/*
     if (message.content === 'ping') {
     	message.reply('pong');
 		}
-		/*
     if (message.content === 'おはよ') {
     	message.reply('うさぎ_(:3 」∠)');
 		}
-		*/
     if (message.content === 'こんにちは') {
     	message.reply('わにわに_(:3 」∠)');
   	}
     if (message.content === 'こんばんは') {
     	message.reply('わにわに_(:3 」∠)');
 		}
-		if(message.content.startswith('おはよ')) {
-    	message.reply('うさぎ_(:3 」∠)');
-		}
+		*/
 });
 
 // THIS  MUST  BE  THIS  WAY
