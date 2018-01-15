1536d998e3825f90d1a356f98155a4e923c0ed66
tentendayoooon <coreten59@gmail.com>
2018-01-15 11:57:25 +0900

    aiueo4

--- bot.js
+++ bot.js
@@ -6,34 +6,19 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-		String msg = message.content;
-    if (msg === 'ping') {
-    	message.reply('pong');
-		}
-    if (msg === 'おはよ') {
-    	message.reply('うさぎ_(:3 」∠)');
-		}
-    if (msg === 'こんにちは') {
-    	message.reply('わにわに_(:3 」∠)');
-  	}
-    if (msg === 'こんばんは') {
-    	message.reply('わにわに_(:3 」∠)');
-		}
-
-		/*
-    if (message.content === 'ping') {
-    	message.reply('pong');
-		}
-    if (message.content === 'おはよ') {
-    	message.reply('うさぎ_(:3 」∠)');
-		}
-    if (message.content === 'こんにちは') {
-    	message.reply('わにわに_(:3 」∠)');
-  	}
-    if (message.content === 'こんばんは') {
-    	message.reply('わにわに_(:3 」∠)');
-		}
-		*/
+	const msg = message.content;
+	if (msg === 'ping') {
+		message.reply('pong');
+	}
+	if (msg === 'おはよ') {
+		message.reply('うさぎ_(:3 」∠)');
+	}
+	if (msg === 'こんにちは') {
+		message.reply('わにわに_(:3 」∠)');
+	}
+	if (msg === 'こんばんは') {
+		message.reply('わにわに_(:3 」∠)');
+	}
 });
 
 // THIS  MUST  BE  THIS  WAY
