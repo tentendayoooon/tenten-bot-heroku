7736a13b2cec966135cd869d7fe584f87199d86a
tentendayoooon <coreten59@gmail.com>
2018-11-12 10:45:12 +0900

    20181112-05

--- bot.js
+++ bot.js
@@ -243,8 +243,8 @@ client.on('message', message => {
 
 	if (msg === 'あのなあ') {
 		message.channel.send('あのなあ　やるよ');
-		const attachment = new MessageAttachment('./stamp/あのなあ.png');
-		message.channel.send(`${message.author}, here are your memes!`, attachment);
+		//const attachment = new MessageAttachment('./stamp/あのなあ.png');
+		//message.channel.send(`${message.author}, here are your memes!`, attachment);
 	}
 
 	if (msg.indexOf('!ガチャ') !== -1) {
